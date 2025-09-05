8439abb839611132c03a19a22c6c9702697a0a55
k-penchev <kaloyan.penchev09@gmail.com>
2025-09-05 17:39:39 +0300

    lca problem

--- 03.Trees/04.Binary-Jumping/lca/p5.cpp
+++ 03.Trees/04.Binary-Jumping/lca/p5.cpp
@@ -2,17 +2,46 @@
 
 using namespace std;
 
-const int MAXQ = 1e5 + 10;
-const int MAXLOG = 17 + 1;
+#define int long long
+
+const int MAXQ = 2 * 1e5 + 10;
+const int MAXLOG = 20 + 1;
 
 int q;
 int depth[MAXQ];
 int lift[MAXQ][MAXLOG];
 int nodesCnt = 0;
+int root[MAXQ], A[MAXQ], B[MAXQ], diameter[MAXQ];
 
 int lca(int a, int b)
 {
+    if(depth[a] < depth[b]) swap(a, b);
 
+    int k = depth[a] - depth[b];
+
+    for(int i = 0 ; i < MAXLOG ; ++i)
+    {
+        if(k & (1 << i))
+        {
+            a = lift[a][i];
+        }
+    }
+
+    if(a == b)
+    {
+        return a;
+    }
+
+    for(int i = MAXLOG - 1 ; i >= 0 ; --i)
+    {
+        if(lift[a][i] != lift[b][i])
+        {
+            a = lift[a][i];
+            b = lift[b][i];
+        }
+    }
+    
+    return lift[a][0];
 }
 
 int dist(int a, int b)
@@ -28,6 +57,10 @@ void update(int par)
 
     if(par == -1)
     {
+        root[currentNode] = currentNode;
+        A[currentNode] = B[currentNode] = root[currentNode];
+        diameter[currentNode] = 0;
+
         for(int i = 0 ; i < MAXLOG ; ++i)
         {
             lift[currentNode][i] = 0;
@@ -46,16 +79,37 @@ void update(int par)
         }
 
         depth[currentNode] = depth[par] + 1;
+        root[currentNode] = root[par];
+
+        int componentRoot = root[currentNode];
+
+        int dist1 = dist(currentNode, A[componentRoot]);
+        int dist2 = dist(currentNode, B[componentRoot]);
+
+        if(dist1 > diameter[componentRoot])
+        {
+            B[componentRoot] = currentNode;
+            diameter[componentRoot] = dist1;
+        }
+        else if(dist2 > diameter[componentRoot])
+        {
+            A[componentRoot] = currentNode;
+            diameter[componentRoot] = dist2;
+        }
     }
 }
 
 int query(int node)
 {
-
+    int componenentRoot = root[node];
+    return max(dist(node, A[componenentRoot]), dist(node, B[componenentRoot]));
 }
 
 void solve()
 {
+    freopen("newbarn.in", "r", stdin);
+    freopen("newbarn.out", "w", stdout);
+
     cin >> q;
 
     for(int i = 1 ; i <= q ; ++i)
@@ -67,11 +121,11 @@ void solve()
 
         if(qType == 'B')
         {
-
+            update(x);
         }
         else
         {
-
+            cout << query(x) << "\n";
         }
     }
 }
@@ -83,7 +137,7 @@ void fastIO()
     cout.tie(NULL);
 }
 
-int main()
+signed main()
 {
     fastIO();
     solve();
