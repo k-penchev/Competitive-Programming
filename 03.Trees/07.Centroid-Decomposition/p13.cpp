3f0a43ca31b2e233345a5847704e12a59033575d
k-penchev <kaloyan.penchev09@gmail.com>
2025-09-19 11:54:47 +0300

    new problems

--- 03.Trees/07.Centroid-Decomposition/p13.cpp
+++ 03.Trees/07.Centroid-Decomposition/p13.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
 
-#define int long long
-
 const int MAXN = 3 * 1e5 + 10;
 const int MAXLOG = 20 + 1;
 const int INF = 1e9;
@@ -17,22 +15,22 @@ namespace LCA
     int in[MAXN], out[MAXN], timer = 0;
     int up[MAXN][MAXLOG], depth[MAXN];
 
-    void dfs(int node, int par, int currDepth)
+    void tour(int node, int par, int currDepth)
     {
         in[node] = ++timer;
         depth[node] = currDepth;
         up[node][0] = par;
 
-        for(int i = 1 ; i < MAXLOG ; ++i)
+        for (int i = 1 ; i < MAXLOG ; ++i)
         {
             int prev = up[node][i - 1];
             up[node][i] = up[prev][i - 1];
         }
 
-        for(const int& child : tree[node])
+        for (const int& child : tree[node])
         {
             if(child == par) continue;
-            dfs(child, node, currDepth + 1);
+            tour(child, node, currDepth + 1);
         }
 
         out[node] = timer;
@@ -40,23 +38,27 @@ namespace LCA
 
     void build()
     {
-        dfs(1, 0, 0);
+        tour(1, 0, 0);
     }
 
-    bool is_ancestor(int a, int b)
+    bool is_anc(int a, int b)
     {
         return (in[a] <= in[b] && out[a] >= out[b]);
     }
 
     int lca(int a, int b)
     {
-        if(is_ancestor(a, b)) return a;
-        if(is_ancestor(b, a)) return b;
+        if(is_anc(a, b)) return a;
+        if(is_anc(b, a)) return b;
 
-        for(int i = MAXLOG - 1 ; i >= 0 ; --i)
+        for (int i = MAXLOG - 1 ; i >= 0 ; --i)
         {
-            int anc = up[a][i];
-            if(anc != 0 && !is_ancestor(anc, b)) a = anc;
+            int nxt = up[a][i];
+
+            if (nxt != 0 && !is_anc(nxt, b))
+            {
+                a = nxt;
+            }
         }
 
         return up[a][0];
@@ -74,17 +76,17 @@ namespace Centroid
     bool vis[MAXN];
     int centroidPar[MAXN][MAXLOG];
     int centroidDist[MAXN][MAXLOG];
-
-    std::vector<int> centroidRadius[MAXN];
-    std::vector<int> childRadius[MAXN];
+    std::vector<int> centroidRadius[MAXN];  // the centroid values itself
+    std::vector<int> childRadius[MAXN];     // the values for the parentCentroid 
 
     void findSize(int node, int par)
     {
         sz[node] = 1;
 
-        for(const int& child : tree[node])
+        for (const int& child : tree[node])
         {
-            if(child == par || vis[child]) continue;
+            if (child == par || vis[child]) continue;
+
             findSize(child, node);
             sz[node] += sz[child];
         }
@@ -92,43 +94,43 @@ namespace Centroid
 
     int findCentroid(int node, int par, int globalSize)
     {
-        for(const int& child : tree[node])
+        for (const int& child : tree[node])
         {
             if(child == par || vis[child]) continue;
+
             if(sz[child] * 2 > globalSize) return findCentroid(child, node, globalSize);
         }
+
         return node;
     }
 
-    void stampLevel(int node, int par, int centroid, int currDist, int level)
+    void calculateDist(int node, int par, int currDist, int globalCentroid, int globalLevel)
     {
-        centroidPar[node][level] = centroid;
-        centroidDist[node][level] = currDist;
+        centroidPar[node][globalLevel] = globalCentroid;
+        centroidDist[node][globalLevel] = currDist;
 
-        for(const int& child : tree[node])
+        for (const int& child : tree[node])
         {
             if(child == par || vis[child]) continue;
-            stampLevel(child, node, centroid, currDist + 1, level);
+            calculateDist(child, node, currDist + 1, globalCentroid, globalLevel);
         }
     }
 
-    void decompose(int node, int level)
+    void decompose(int node, int lvl)
     {
         findSize(node, 0);
         int cntr = findCentroid(node, 0, sz[node]);
 
         vis[cntr] = 1;
+        calculateDist(cntr, 0, 0, cntr, lvl);
+        
+        centroidRadius[cntr].resize(sz[node]);
+        childRadius[cntr].resize(sz[node] + 1);
 
-        stampLevel(cntr, 0, cntr, 0, level);
-
-        // size by component size (safe bound)
-        centroidRadius[cntr].assign(sz[cntr], 0);
-        childRadius[cntr].assign(sz[cntr] + 1, 0);
-
-        for(const int& child : tree[cntr])
+        for (const int& child : tree[cntr])
         {
-            if(vis[child]) continue;
-            decompose(child, level + 1);
+            if (vis[child]) continue;
+            decompose(child, lvl + 1);
         }
     }
 
@@ -137,95 +139,87 @@ namespace Centroid
         decompose(1, 0);
     }
 
-    void update(int node, int radius)
-    {
-        if(radius < 0) return;
-
-        int prev = -1;
-        for(int level = 0 ; level < MAXLOG ; ++level)
-        {
-            int cntr = centroidPar[node][level];
-            if(cntr == 0) break;
-
-            int d = centroidDist[node][level];
-            int rem = radius - d;
-            if(rem >= 0)
-            {
-                int i1 = rem;
-                if(i1 >= (int)centroidRadius[cntr].size()) i1 = (int)centroidRadius[cntr].size() - 1;
-                if(i1 >= 0) centroidRadius[cntr][i1] += 1;
-
-                if(prev != -1)
-                {
-                    int i2 = rem;
-                    if(i2 >= (int)childRadius[prev].size()) i2 = (int)childRadius[prev].size() - 1;
-                    if(i2 >= 0) childRadius[prev][i2] += 1;
-                }
-            }
-
-            prev = cntr;
-        }
-    }
-
     void propagate()
     {
-        for(int c = 1 ; c <= n ; ++c)
+        for (int i = 1 ; i <= n ; ++i)
         {
-            if(!centroidRadius[c].empty())
+            int m = centroidRadius[i].size();
+
+            for (int j = m - 2 ; j >= 0 ; --j)
             {
-                for(int j = (int)centroidRadius[c].size() - 2 ; j >= 0 ; --j)
-                {
-                    centroidRadius[c][j] += centroidRadius[c][j + 1];
-                }
+                centroidRadius[i][j] += centroidRadius[i][j + 1];
             }
-            if(!childRadius[c].empty())
+
+            m = childRadius[i].size();
+
+            for (int j = m - 2 ; j >= 0 ; --j)
             {
-                for(int j = (int)childRadius[c].size() - 2 ; j >= 0 ; --j)
-                {
-                    childRadius[c][j] += childRadius[c][j + 1];
-                }
+                childRadius[i][j] += childRadius[i][j + 1];
             }
         }
+        
     }
+
+    void update(int node, int R)
+    {
+        if (R < 0) return;
+        
+        int prev = -1;
+        
+        for (int lvl = 0; lvl < MAXLOG; ++lvl)
+        {            
+            int cntr = centroidPar[node][lvl];
+            
+            if (cntr == 0) break;                                     
+            
+            int d = centroidDist[node][lvl];
+            int raw = R - d;
+            
+            if (raw < 0) continue;
+
+            int idxC = std::min(raw, (int)centroidRadius[cntr].size() - 1);
+            centroidRadius[cntr][idxC] += 1;
+
+            if (prev != -1)
+            {
+                int idxP = std::min(raw, (int)childRadius[prev].size() - 1);
+                childRadius[prev][idxP] += 1;
+            }
+            prev = cntr;
+
+        }
+    }
+
 
     int query(int node)
     {
-        int s = 0;
-        int prevCntr = -1;
-
-        for(int level = 0 ; level < MAXLOG ; ++level)
+        int s = 0, prev = -1;
+        
+        for (int lvl = 0; lvl < MAXLOG; ++lvl)
         {
-            int cntr = centroidPar[node][level];
-            if(cntr == 0) break;
-
-            int d = centroidDist[node][level];
-
-            if(!centroidRadius[cntr].empty())
-            {
-                int idx = d;
-                if(idx >= (int)centroidRadius[cntr].size()) idx = (int)centroidRadius[cntr].size() - 1;
-                s += centroidRadius[cntr][idx];
-            }
-
-            if(prevCntr != -1 && !childRadius[prevCntr].empty())
-            {
-                int idx2 = d;
-                if(idx2 >= (int)childRadius[prevCntr].size()) idx2 = (int)childRadius[prevCntr].size() - 1;
-                s -= childRadius[prevCntr][idx2];
-            }
-
-            prevCntr = cntr;
+            int cntr = centroidPar[node][lvl];
+            
+            if (cntr == 0) break;
+            
+            int d = centroidDist[node][lvl];
+            
+            if (d < (int)centroidRadius[cntr].size()) s += centroidRadius[cntr][d];
+            if (prev != -1 && d < (int)childRadius[prev].size()) s -= childRadius[prev][d];
+            
+            prev = cntr;
         }
-
+        
         return s;
     }
+
 };
 
 void dfs1(int node, int par)
 {
-    for(const int& child : tree[node])
+    for (const int& child : tree[node])
     {
-        if(child == par) continue;
+        if (child == par) continue;
+
         dfs1(child, node);
         shopDist[node] = std::min(shopDist[node], shopDist[child] + 1);
     }
@@ -233,9 +227,10 @@ void dfs1(int node, int par)
 
 void dfs2(int node, int par)
 {
-    for(const int& child : tree[node])
+    for (const int& child : tree[node])
     {
-        if(child == par) continue;
+        if (child == par) continue;
+
         shopDist[child] = std::min(shopDist[child], shopDist[node] + 1);
         dfs2(child, node);
     }
@@ -244,7 +239,10 @@ void dfs2(int node, int par)
 void solve()
 {
     std::fill(shopDist + 1, shopDist + n + 1, INF);
-    for(int i = 1 ; i <= k ; ++i) shopDist[shop[i]] = 0;
+    for (int i = 1 ; i <= k ; ++i)
+    {
+        shopDist[shop[i]] = 0;
+    }
 
     dfs1(1, 0);
     dfs2(1, 0);
@@ -252,20 +250,31 @@ void solve()
     LCA::build();
     Centroid::build();
 
-    for(int i = 1 ; i <= n ; ++i) favDist[i] = LCA::dist(i, f[i]);
-
-    for(int i = 1 ; i <= n ; ++i)
+    for (int i = 1 ; i <= n ; ++i)
     {
-        int radius = std::min(shopDist[i], favDist[i] - p[i] - 1);
-        Centroid::update(i, radius);
+        favDist[i] = LCA::dist(i, f[i]);
+    }
+
+    for (int i = 1 ; i <= n ; ++i)
+    {
+        if (favDist[i] <= shopDist[i] + p[i])
+        {
+            Centroid::update(i, favDist[i] - p[i] - 1);
+        }
+        else
+        {
+            Centroid::update(i, shopDist[i]);
+        }
+        
     }
 
     Centroid::propagate();
 
-    for(int i = 1 ; i <= n ; ++i)
+    for (int i = 1 ; i <= n ; ++i)
     {
         std::cout << Centroid::query(i) << " ";
     }
+
     std::cout << "\n";
 }
 
@@ -273,16 +282,23 @@ void input()
 {
     std::cin >> n >> k;
 
-    for(int i = 1 ; i <= n - 1 ; ++i)
+    for (int i = 1 ; i <= n - 1 ; ++i)
     {
-        int a, b;
+        int a, b; 
         std::cin >> a >> b;
         tree[a].push_back(b);
         tree[b].push_back(a);
     }
 
-    for(int i = 1 ; i <= k ; ++i) std::cin >> shop[i];
-    for(int i = 1 ; i <= n ; ++i) std::cin >> f[i] >> p[i];
+    for (int i = 1 ; i <= k ; ++i)
+    {
+        std::cin >> shop[i];
+    }
+
+    for (int i = 1 ; i <= n ; ++i)
+    {
+        std::cin >> f[i] >> p[i];
+    }
 }
 
 void fastIO()
@@ -292,7 +308,7 @@ void fastIO()
     std::cout.tie(NULL);
 }
 
-signed main()
+int main()
 {
     fastIO();
     input();
