655f89edf98868291d8ca98f8fd0bc3f0d20ce54
k-penchev <kaloyan.penchev09@gmail.com>
2025-10-22 22:40:56 +0300

    changed the file

--- Arena/05.Divide-and-conquer/1.cpp
+++ Arena/05.Divide-and-conquer/1.cpp
@@ -1,59 +1,11 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <cassert>
 
-const int MAXN = 128;
+const int MAXN = 128 + 5;
 
 int n;
 int a[MAXN];
-std::vector<int> swaps;
-
-bool isSorted()
-{
-    for(int i = 1 ; i <= n ; ++i)
-    {
-        for(int j = i + 1 ; j <= n ; ++j)
-        {
-            if(a[i] > a[j]) return false;
-        }
-    }
-
-    return true;
-}
-
-// a[l ... m] ; a[m + 1 ... r]
-void conquer(int l, int m, int r)
-{
-    int ptr = 2;
-    while(!isSorted())
-    {
-        int current = a[ptr];
-
-        for(int i = ptr ; i >= l ; --i)
-        {
-            a[i] = a[i - 1];
-        }
-
-        a[l] = current;
-        swaps.push_back(ptr);
-        
-        ptr <<= 1;
-        if(ptr > r) ptr = 2;
-    }
-}
-
-void divide(int l, int r)
-{
-    if(l >= r) return;
-
-    int m = (l + r) / 2;
-
-    divide(l, m);
-    divide(m + 1, r);
-
-    conquer(l, m, r);
-}
 
 void solve()
 {
@@ -64,16 +16,7 @@ void solve()
         std::cin >> a[i];
     }
 
-    divide(1, n);
-
-    std::cout << swaps.size() << "\n";
-
-    for(int s : swaps)
-    {
-        std::cout << s << " ";
-    }
-
-    std::cout << "\n";
+    
 }
 
 void fastIO()
